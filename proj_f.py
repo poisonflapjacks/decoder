0ce2022a33b7a4d74e2fa65c302ba582ff9a5848
micheal chapa <100226197+poisonflapjacks@users.noreply.github.com>
2024-06-03 15:36:58 -0500

    Update proj_f.py

--- proj_f.py
+++ proj_f.py
@@ -1,4 +1,6 @@
 import math
+import random
+from wonderwords import RandomWord
 
 def is_perfect_square(num):
 
@@ -6,11 +8,10 @@ def is_perfect_square(num):
     return sqrtt.is_integer()
 
 def is_triangular(num):
+
     numb = int(num)
     return is_perfect_square(8 * numb + 1)
 
-
-# TODO Needs cleaner output
 def decode_triangular():
 
     output_arr = []
@@ -18,24 +19,18 @@ def decode_triangular():
     with open('encoded_message.txt', 'r') as File:
         
         for line in File:
+
             full_line = line.split()
             
             if is_triangular( int(full_line[0]) ):
-
                 output_arr.append([ int(full_line[0]), full_line[1]])
 
     output_arr.sort()
-    
-    # TODO convert array to nicer print
 
     print(output_arr)
 
-###
-
-# RN this file expands a given input into a list
-# that has "text" for words that are not triangular
-
 def create_fake_list(message):
+
     full_list = []
     full_fake_list = []
     z = 0
@@ -44,6 +39,7 @@ def create_fake_list(message):
     message_len = len(message_as_list)
 
     for x in range(message_len):
+
         z = int(((x + 1)*(x + 2))/2)
         full_list.append([str(z), message_as_list[x]])
 
@@ -54,28 +50,37 @@ def create_fake_list(message):
             full_fake_list.append(added)
 
         else:
-            full_fake_list.append([str(y+1), "Text"])
+            RandW = RandomWord()
+            full_fake_list.append([str(y+1), RandW.word()])
 
     return full_fake_list
 
-# def scramble():
- #
-   # with open('test_file.txt', 'r+') as File:
+#TODO: Fix scramble poriton
+    # def scramble(file_size):
+
+    #with open('encoded_message.txt', 'r+') as File:
+
+       # for i in range( file_size/2 ):
+
+        #    File.write(line)
 
 def create_encoded_file(message):
+
+    file_size = 0
 
     with open('encoded_message.txt', 'w') as File:
 
         full_fake_list = create_fake_list(message)
+        file_size = len(full_fake_list)
 
-        for i in range(len(full_fake_list)):
+        for i in range( file_size ):
 
-            line = full_fake_list[i][0] + " " + full_fake_list[i][1] + "\n"\
-
+            line = full_fake_list[i][0] + " " + full_fake_list[i][1] + "\n"
             File.write(line)
-            #print(line)
 
-def prompt():
+    #scramble(file_size)
+
+def main():
 
     message = input("Enter your phrase: ")
 
@@ -84,4 +89,4 @@ def prompt():
     create_encoded_file(message)
     decode_triangular()
 
-prompt()
+main()
